f2da28b5f44cdda39e10feb3d7017f935c666c76
panasonikk <grebenyuk1507@gmail.com>
2025-02-24 15:11:58 +0200

    Initial commit

---
+++ models/database.js
@@ -0,0 +1,33 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+}).promise();
+
+export async function getBooks() {
+    const dbResult = await pool.query("SELECT * FROM books");
+    const booksList = dbResult[0];
+    return booksList;
+}
+
+export async function getBookById(id) {
+    const [bookById] = await pool.query(`
+        SELECT * 
+        FROM books
+        WHERE id = ?
+        `, [id]); //prepared statement
+    return bookById[0];
+}
+
+export async function addBook(title, author, description, imageUrl, genre, numberOfPages, language, dateOfPublishing) {
+    const [newBook] = await pool.query(`
+    INSERT INTO books (title, author, description, imageUrl, genre, numberOfPages, language, dateOfPublishing)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?) 
+    `, [title, author, description, imageUrl, genre, numberOfPages, language, dateOfPublishing]);
+    return getBookById(newBook.insertId);
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import {getBooks, getBookById, addBook} from './models/database.js';
+import {calculateDecisionMatrix} from './utils/decisionMatrix.js';
+
+const app = express();
+app.use(express.json());
+
+app.get("/books/top", async (req, res) => {
+    const books = await getBooks();
+    const rankedBooks = await calculateDecisionMatrix(books);
+    const topBooks = rankedBooks.slice(0, 2);
+    res.send(topBooks);
+});
+
+app.get("/books", async (req, res) => {
+    const books = await getBooks();
+    res.send(books);
+});
+
+app.get("/books/:id", async (req, res) => {
+    const id = req.params.id;
+    const book = await getBookById(id);
+    res.send(book);
+});
+
+
+
+
+app.post("/books", async (req, res) => {
+    const {title, author, description, imageUrl, genre, numberOfPages, language, dateOfPublishing} = req.body;
+    const newBook = await addBook(title, author, description, imageUrl, genre, numberOfPages, language, dateOfPublishing);
+    res.status(201).send(newBook);
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
